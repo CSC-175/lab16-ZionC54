2b24972984208988044158eb736fd97434d508fd
Zion Clingman <zionalex@umich.edu>
2024-11-14 17:15:17 -0500

    First Attempt

--- lab16.cpp
+++ lab16.cpp
@@ -1,1 +1,16 @@
 // Code to implement the gcf function goes here
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int gcf(int a, int b) {
+    if (b == 0) {
+        return a;
+    }
+    else if (a == 0) {
+        return b;
+    }
+    else {
+        return gcf(b, a % b);
+    }
+}
